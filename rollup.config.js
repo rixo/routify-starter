725e3df2e470dceb84b6fbfffe4cc1d2d0c03af3
rixo <rixo@rixo.fr>
2020-03-29 16:25:45 +0200

    migrating to beta

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,42 @@
-import svelte from 'rollup-plugin-svelte-hot';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import { config } from '@sveltech/routify'
+import svelte from 'rollup-plugin-svelte-hot'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
+// import { getConfig } from '@sveltech/routify'
 import copy from 'rollup-plugin-copy'
 import del from 'del'
 import ppidChanged from 'ppid-changed'
 import hmr from 'rollup-plugin-hot'
+import { routify } from '@sveltech/routify'
 
+const production = !process.env.ROLLUP_WATCH
+const split = !!process.env.SPLIT
 
-const production = !process.env.ROLLUP_WATCH;
-const { distDir, staticDir, sourceDir, dynamicImports: split } = config
+// const config = {
+//   "pages": "/home/eric/projects/routify/svench/svench/example/src",
+//   "sourceDir": "public",
+//   "routifyDir": "node_modules/@sveltech/routify/tmp",
+//   "ignore": [],
+//   "dynamicImports": false,
+//   "singleBuild": false,
+//   "noHashScroll": false,
+//   "extensions": [
+//     "svench",
+//     "svench.svx",
+//     "svhx"
+//   ],
+//   "distDir": "dist",
+//   "noPrerender": false,
+//   "watch": "true",
+//   "watchDelay": 20
+// }
+
+const distDir = 'dist'
+const sourceDir = 'public'
+const staticDir = 'static'
+
+// const { distDir, staticDir, sourceDir } = config
 const buildDir = `${distDir}/build`
 const template = staticDir + (split ? '/__dynamic.html' : '/__bundled.html')
 const hot = !production
@@ -21,70 +46,76 @@ const hot = !production
 if (ppidChanged()) del.sync(distDir + '/**')
 
 export default {
-	input: `${sourceDir}/main.js`,
-	output: [{
-		sourcemap: true,
-		name: 'app',
-		...split ? {
-			format: 'esm',
-			dir: buildDir,
-		} : {
-			format: 'iife',
-			file: `${buildDir}/bundle.js`
-		}
-	}],
-	plugins: [
-		copy({
-			targets: [
-			  { src: staticDir + '/*', dest: distDir },
-			  { src: template, dest: distDir, rename: '__app.html' },
-			], copyOnce: true
-		  }),
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			hydratable: true,
-			// NOTE CSS file extraction is not supported with HMR
-			...hot ? {
-				hot: {
-					// optimistic will try to recover from runtime
-					// errors during component init
-					optimistic: true,
-					// turn on to disable preservation of local component
-					// state -- i.e. non exported `let` variables
-					noPreserveState: false,
-				},
-			} : {
-				// we'll extract any component CSS out into
-				// a separate file — better for performance
-				css: css => {
-					css.write(`${buildDir}/bundle.css`);
-				},
-			}
-		}),
+  input: `${sourceDir}/main.js`,
+  output: [
+    {
+      sourcemap: true,
+      name: 'app',
+      format: split ? 'esm' : 'iife',
+      [split ? 'dir' : 'file']: split ? `${buildDir}` : `${buildDir}/bundle.js`,
+    },
+  ],
+  plugins: [
+    copy({
+      targets: [
+        { src: staticDir + '/*', dest: distDir },
+        { src: template, dest: distDir, rename: '__app.html' },
+      ],
+      copyOnce: true,
+    }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
-		commonjs(),
+    routify({
+      dynamicImports: split,
+    }),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      hydratable: true,
+      // NOTE CSS file extraction is not supported with HMR
+      ...(hot
+        ? {
+            hot: {
+              // optimistic will try to recover from runtime
+              // errors during component init
+              optimistic: true,
+              // turn on to disable preservation of local component
+              // state -- i.e. non exported `let` variables
+              noPreserveState: false,
+            },
+          }
+        : {
+            // we'll extract any component CSS out into
+            // a separate file — better for performance
+            css: css => {
+              css.write(`${buildDir}/bundle.css`)
+            },
+          }),
+    }),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && !hot && livereload(distDir),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === 'svelte' || importee.startsWith('svelte/'),
+    }),
+    commonjs(),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser(),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload(distDir),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
 
     hmr({
       public: distDir,
@@ -95,25 +126,25 @@ export default {
       // nothing else.
       compatModuleHot: !hot,
     }),
-	],
-	watch: {
-		clearScreen: false
-	}
+  ],
+  watch: {
+    clearScreen: false,
+  },
 }
 
 function serve() {
-	let started = false;
+  let started = false
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true,
+        })
+      }
+    },
+  }
 }
